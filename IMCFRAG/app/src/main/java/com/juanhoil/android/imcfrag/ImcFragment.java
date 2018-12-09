cda12c0cafce99278223746459c583a2abf7bd2a
15470289@itcampeche.edu.mx <15470289@itcampeche.edu.mx>
2018-12-09 16:42:53 -0600

    aplicacion  que calcula el imc utilizando fragmentos de ui

---
+++ IMCFRAG/app/src/main/java/com/juanhoil/android/imcfrag/ImcFragment.java
@@ -0,0 +1,23 @@
+package com.juanhoil.android.imcfrag;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ImcFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView( LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_imc,container,false);
+    return  v;
+    }
+}
