d24cf4b5f03aa9c8e18f8aa3bb3bd48919b96344
15470289@itcampeche.edu.mx <15470289@itcampeche.edu.mx>
2018-12-09 21:33:06 -0600

    aplicacion criminalintent que se hizo en clase

---
+++ CriminalIntent/app/src/main/java/com/juanhoil/android/crimanalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.juanhoil.android.crimanalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
